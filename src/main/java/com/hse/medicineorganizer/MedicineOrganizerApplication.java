6ae8e077e6fe9226d89b42828d50e63227a46f65
Dmitriy <45269664+drdolgosheev@users.noreply.github.com>
2020-11-28 19:18:23 +0300

    Add files via upload

---
+++ src/main/java/com/hse/medicineorganizer/MedicineOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.hse.medicineorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineOrganizerApplication.class, args);
+	}
+
+}
